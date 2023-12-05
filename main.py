b83ad4e2e2591c6726abc3882ed38aa8dde8a600
javiermolinapp <123014175+javiermolinapp@users.noreply.github.com>
2023-12-05 16:06:43 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from flask import Flask, jsonify
-import os
+import io
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot
+from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS, cross_origin
+import base64
+
+
+
 
 app = Flask(__name__)
 
 
+CORS(app)
 
-
+@cross_origin
+########################GRAFICO DE LINEAS
 @app.route("/graficarlinea/<x1>/<x2>")
 def generate_image2(x1, x2):
     return x1+x2;
 
 
+
+    
+
+def create_app():
+    return app
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=8000)
+
+
+
+    
+    
+    
