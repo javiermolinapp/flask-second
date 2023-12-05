e31a2b940bac5389766d3ad1a7726111c793dae0
javiermolinapp <123014175+javiermolinapp@users.noreply.github.com>
2023-12-05 16:03:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+@app.route("/graficarlinea/<x1>/<x2>")
+def generate_image2(x1, x2):
+    return x1+x2;
 
 
 if __name__ == '__main__':
